446acb99a7b5264c1f032ab3c932affa446eecab
sang <sangng0166@gmai.com>
2025-06-30 00:27:15 +0700

    reset

--- divide.js
+++
@@ -1,8 +0,0 @@
-function calculate(sign, arg1, arg2) {
-    switch (sign) {
-        case "DIVIDE":
-            return arg2 !== 0 ? arg1 / arg2 : null;
-        default:
-            return 0;
-    }
-}
